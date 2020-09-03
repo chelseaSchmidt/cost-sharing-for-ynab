f8628a264313d46b2423c7b4f2b6d794cfd9d19e
Chelsea Schmidt <cschmidt331@gmail.com>
2020-09-02 21:05:45 -0700

    Factor out http requests to be DRY

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import axios from 'axios';
-import { token, budgetId, ccId, dueToFromId } from '../../../identifiers';
+import { getCCTransactions, getDTFTransactions } from '../../utilities/http';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,34 +11,15 @@ export default class App extends React.Component {
     };
   }
 
-  getFiveDaysAgo() {
-    let day = new Date();
-    return new Date(day.setDate(day.getDate() - 5));
-  }
-
   componentDidMount() {
     const { sinceDate } = this.state;
     let ccTransactions = [];
     let dueToFromTransactions = [];
 
-    axios({
-      method: 'get',
-      headers: { Authorization: `Bearer ${token}` },
-      url: `https://api.youneedabudget.com/v1/budgets/${budgetId}/accounts/${ccId}/transactions`,
-      params: {
-        since_date: `${sinceDate}`,
-      },
-    })
+    getCCTransactions(sinceDate)
       .then(({ data: transactions }) => {
         ccTransactions = transactions;
-        return axios({
-          method: 'get',
-          headers: { Authorization: `Bearer ${token}` },
-          url: `https://api.youneedabudget.com/v1/budgets/${budgetId}/accounts/${dueToFromId}/transactions`,
-          params: {
-            since_date: `${sinceDate}`,
-          },
-        });
+        return getDTFTransactions(sinceDate);
       })
       .then(({ data: transactions }) => {
         dueToFromTransactions = transactions;
@@ -48,14 +28,19 @@ export default class App extends React.Component {
       .catch((err) => { console.error(err); });
   }
 
+  getFiveDaysAgo() {
+    const day = new Date();
+    return new Date(day.setDate(day.getDate() - 5));
+  }
+
   render() {
     const { ccTransactions, dueToFromTransactions } = this.state;
     return (
       <div>
-        <label htmlFor='start'>Specify start date:</label>
-        <input type='date' id='start' />
-        <label htmlFor='start'>Specify end date:</label>
-        <input type='date' id='end' />
+        <label htmlFor="start">Specify start date:</label>
+        <input type="date" id="start" />
+        <label htmlFor="start">Specify end date:</label>
+        <input type="date" id="end" />
         <div>
           {JSON.stringify(ccTransactions)}
         </div>
---
+++ client/utilities/http.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import {
+  token,
+  budgetId,
+  ccId,
+  dueToFromId,
+} from '../../identifiers';
+
+export const getCCTransactions = (sinceDate) => (
+  axios({
+    method: 'get',
+    headers: { Authorization: `Bearer ${token}` },
+    url: `https://api.youneedabudget.com/v1/budgets/${budgetId}/accounts/${ccId}/transactions`,
+    params: {
+      since_date: `${sinceDate}`,
+    },
+  })
+);
+
+export const getDTFTransactions = (sinceDate) => (
+  axios({
+    method: 'get',
+    headers: { Authorization: `Bearer ${token}` },
+    url: `https://api.youneedabudget.com/v1/budgets/${budgetId}/accounts/${dueToFromId}/transactions`,
+    params: {
+      since_date: `${sinceDate}`,
+    },
+  })
+);
