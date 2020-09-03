b89c05607a196b35f38617ab59b8077662cb0ac1
Chelsea Schmidt <cschmidt331@gmail.com>
2020-09-02 18:02:04 -0700

    Initial setup

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3003;
+const publicDir = path.resolve(__dirname, '..', 'client', 'public');
+
+app.use(morgan('dev'));
+app.use(express.static(publicDir));
+
+app.listen(port, () => {
+  console.log(`Good to go at port ${port}`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client', 'public'),
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
