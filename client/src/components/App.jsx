4675779074fd2b70fd16aa81d65b87b1ef267f95
Chelsea Schmidt <cschmidt331@gmail.com>
2020-09-02 20:41:19 -0700

    Add HTTP request to retrieve transactions from expense splitting account

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import axios from 'axios';
-import { token, budgetId } from '../../../identifiers';
+import { token, budgetId, ccId, dueToFromId } from '../../../identifiers';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       sinceDate: this.getFiveDaysAgo(),
-      transactions: [],
+      ccTransactions: [],
+      dueToFromTransactions: [],
     };
   }
 
@@ -18,25 +19,45 @@ export default class App extends React.Component {
 
   componentDidMount() {
     const { sinceDate } = this.state;
+    let ccTransactions = [];
+    let dueToFromTransactions = [];
+
     axios({
       method: 'get',
       headers: { Authorization: `Bearer ${token}` },
-      url: `https://api.youneedabudget.com/v1/budgets/${budgetId}/transactions`,
+      url: `https://api.youneedabudget.com/v1/budgets/${budgetId}/accounts/${ccId}/transactions`,
       params: {
         since_date: `${sinceDate}`,
       },
     })
       .then(({ data: transactions }) => {
-        this.setState({ transactions });
+        ccTransactions = transactions;
+        return axios({
+          method: 'get',
+          headers: { Authorization: `Bearer ${token}` },
+          url: `https://api.youneedabudget.com/v1/budgets/${budgetId}/accounts/${dueToFromId}/transactions`,
+          params: {
+            since_date: `${sinceDate}`,
+          },
+        });
+      })
+      .then(({ data: transactions }) => {
+        dueToFromTransactions = transactions;
+        this.setState({ ccTransactions, dueToFromTransactions });
       })
       .catch((err) => { console.error(err); });
   }
 
   render() {
-    const { transactions } = this.state;
+    const { ccTransactions, dueToFromTransactions } = this.state;
     return (
       <div>
-        {JSON.stringify(transactions)}
+        <div>
+          {JSON.stringify(ccTransactions)}
+        </div>
+        <div>
+          {JSON.stringify(dueToFromTransactions)}
+        </div>
       </div>
     );
   }
