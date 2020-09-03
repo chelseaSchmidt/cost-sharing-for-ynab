2bbda64b22f5253efb1c86bb9f1ef442d6d90183
Chelsea Schmidt <cschmidt331@gmail.com>
2020-09-02 20:09:36 -0700

    Set up initial HTTP request with YNAB API to get 5 days of transactions

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import axios from 'axios';
+import { token, budgetId } from '../../../identifiers';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sinceDate: this.getFiveDaysAgo(),
+      transactions: [],
+    };
+  }
+
+  getFiveDaysAgo() {
+    let day = new Date();
+    return new Date(day.setDate(day.getDate() - 5));
+  }
+
+  componentDidMount() {
+    const { sinceDate } = this.state;
+    axios({
+      method: 'get',
+      headers: { Authorization: `Bearer ${token}` },
+      url: `https://api.youneedabudget.com/v1/budgets/${budgetId}/transactions`,
+      params: {
+        since_date: `${sinceDate}`,
+      },
+    })
+      .then(({ data: transactions }) => {
+        this.setState({ transactions });
+      })
+      .catch((err) => { console.error(err); });
+  }
+
   render() {
+    const { transactions } = this.state;
     return (
       <div>
-        Hello
+        {JSON.stringify(transactions)}
       </div>
     );
   }
