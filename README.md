2f15769f62ced341d416651acfdd2c87b866d2a7
Chelsea Schmidt <cschmidt331@gmail.com>
2020-10-13 13:04:34 -0700

    Refactor to use HTTPS server

--- README.md
+++ README.md
@@ -5,9 +5,17 @@
   - npm package manager
   - Nodemon (version 2.0.4 used in development)
 
-## Run the application
+## Run the application in development
   - npm install
-  - npm run build
+  - npm run build || npm run watch
   - npm start
     - Optional environment variable: PORT (defaults to 3003)
   - Navigate to http://localhost:3003
+
+## Other
+  - Command to run ESLint
+    - npm run linter
+  - Command to run HTTP server at port 80
+    - npm run start:HTTP
+  - Command to run HTTPS server at port 443
+    - KEY=`<key file path>` CERT=`<cert file path>` npm run start:HTTPS
--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const morgan = require('morgan');
 const path = require('path');
 
-const app = express();
+const usingHTTPS = !!process.env.HTTPS;
 const port = process.env.PORT || 3003;
 const publicDir = path.resolve(__dirname, '..', 'client', 'public');
+const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static(publicDir));
 
-app.listen(port, () => {
-  console.log(`Good to go at port ${port}`);
-});
+if (usingHTTPS) {
+  const options = {
+    key: fs.readFileSync(process.env.KEY) || '',
+    cert: fs.readFileSync(process.env.CERT) || '',
+  };
+  https.createServer(options, app).listen(port, () => {
+    console.log(`HTTPS good to go at port ${port}`);
+  });
+} else {
+  app.listen(port, () => console.log(`HTTP good to go at port ${port}`));
+}
