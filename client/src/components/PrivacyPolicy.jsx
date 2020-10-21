607284c306bb17ac8e99e57af9b4905b997aa7de
Chelsea Schmidt <cschmidt331@gmail.com>
2020-10-21 14:45:02 -0700

    Update language in privacy policy

--- client/src/components/PrivacyPolicy.jsx
+++ client/src/components/PrivacyPolicy.jsx
@@ -12,22 +12,31 @@ const PrivacyPolicy = ({ setPrivacyActive }) => {
         </p>
         <p>
           This Privacy Policy describes how your personal information is collected,
-          used, and shared when you visit Cost Sharing For YNAB.
+          used, and shared when you visit Cost Sharing for YNAB.
         </p>
         <br />
         <p>
           PERSONAL INFORMATION COLLECTED
         </p>
         <p>
-          Cost Sharing For YNAB does not collect any personal information. Outside
-          of YNAB itself, there is no database connected to Cost Sharing For YNAB
-          in which to store information. Please see YNAB&apos;s privacy policy to
-          understand what information is collected by YNAB, unrelated to
-          using Cost Sharing For YNAB.
-
+          Cost Sharing for YNAB does not collect any personal information. Outside
+          of connecting to YNAB itself through their API, there is no database
+          connected to Cost Sharing for YNAB in which to store information. Please see
+          YNAB&apos;s privacy policy to understand what information is collected by YNAB,
+          unrelated to using Cost Sharing for YNAB.
+        </p>
+        <p>
           Data will be obtained from your YNAB budget for the current active session
-          only, and will not be stored. Cost Sharing For YNAB does not pass the data
-          to any third parties.
+          of Cost Sharing for YNAB only, and will not be stored. Cost Sharing for YNAB
+          does not pass the data to any third parties.
+        </p>
+        <p>
+          Cost Sharing for YNAB is an open-source web application. If you would like to
+          verify any of the above statements, the source-code can be viewed at&nbsp;
+          <a href="https://github.com/chelseaSchmidt/cost-sharing-for-ynab">
+            https://github.com/chelseaSchmidt/cost-sharing-for-ynab
+          </a>
+          .
         </p>
         <br />
         <p>
