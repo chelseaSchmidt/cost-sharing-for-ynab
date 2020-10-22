410f274b3919c57b4a36830421c514feed22b708
Chelsea Schmidt <cschmidt331@gmail.com>
2020-10-21 18:36:42 -0700

    Make app URL relative

--- client/siteSrc/components/Home.jsx
+++ client/siteSrc/components/Home.jsx
@@ -11,8 +11,12 @@ const Home = () => {
       <Header />
       <div id="content-container">
         <div className="column">
-          <a href={`https://app.youneedabudget.com/oauth/authorize?client_id=4ac8ca3c431ac99075e603496136606d7da8102f6178ce2796566b30c4659988&redirect_uri=${url}${appEndpoint}&response_type=token`}>Authenticate and use the app</a>
-          <a href={`${url}cost-sharer`}>Preview the app without authenticating (developer)</a>
+          <a href={`https://app.youneedabudget.com/oauth/authorize?client_id=4ac8ca3c431ac99075e603496136606d7da8102f6178ce2796566b30c4659988&redirect_uri=${url}${appEndpoint}&response_type=token`}>
+            Authenticate and use the app
+          </a>
+          <a href="/cost-sharer">
+            Preview the app without authenticating (developer)
+          </a>
           <PrivacyPolicy />
         </div>
       </div>
