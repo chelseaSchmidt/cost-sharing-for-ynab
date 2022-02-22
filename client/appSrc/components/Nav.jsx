c654c7df0d07a2ee8946a08bda95e31baed65363
Chelsea Schmidt <cschmidt331@gmail.com>
2022-02-21 17:50:06 -0800

    Add email to footer nav links and some related styling

--- client/appSrc/components/Nav.jsx
+++ client/appSrc/components/Nav.jsx
@@ -1,20 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { LinkishButton, Hyperlink } from './styledComponents';
+import breakpoints from '../../shared/breakpoints';
 import modalNames from './modalNames';
+import { LinkishButton, Hyperlink } from './styledComponents';
 
 const Container = styled.div`
   margin-bottom: 15px;
+  font-size: 12px;
+  @media (max-width: ${breakpoints.mobile}) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const DesktopSpacer = styled.span`
+  @media (max-width: ${breakpoints.mobile}) {
+    display: none;
+  }
+`;
+
+const MobileSpacer = styled.div`
+  display: none;
+  @media (max-width: ${breakpoints.mobile}) {
+    display: flex;
+    height: 7px;
+  }
 `;
 
 const Nav = ({ setActiveModal }) => (
   <Container>
     <Hyperlink
+      href="mailto:cost.sharing.for.ynab@gmail.com"
+    >
+      cost.sharing.for.ynab@gmail.com
+    </Hyperlink>
+
+    <DesktopSpacer>·</DesktopSpacer>
+    <MobileSpacer />
+
+    <Hyperlink
       href="/"
     >
       Home
     </Hyperlink>
+
+    <DesktopSpacer>·</DesktopSpacer>
+    <MobileSpacer />
 
     <LinkishButton
       type="button"
@@ -22,6 +55,9 @@ const Nav = ({ setActiveModal }) => (
     >
       Privacy Policy
     </LinkishButton>
+
+    <DesktopSpacer>·</DesktopSpacer>
+    <MobileSpacer />
 
     <Hyperlink
       href="https://github.com/chelseaSchmidt/cost-sharing-for-ynab"
