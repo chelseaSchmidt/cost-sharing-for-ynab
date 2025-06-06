654d06a1b2478d0cb27362f949fd18dd3e95f7b1
Vitor Muzzi Magalhaes <vitormuzzi@gmail.com>
2025-06-05 21:31:06 -0400

    Allow 0%/100% split

--- client/appSrc/components/CostSharingForYnab.jsx
+++ client/appSrc/components/CostSharingForYnab.jsx
@@ -277,19 +277,22 @@ const CostSharingForYnab = () => {
 
   const getClassifiedTransactions = async ({ startDate, endDate }) => {
     const isTransactionATransfer = (transaction) => !!transaction.transfer_account_id;
-
+  
     try {
       setAreTransactionsLoading(true);
       const transactionsSinceStartDate = await getTransactionsSinceDate(startDate);
-
-      const displayedTransactions = transactionsSinceStartDate.filter((transaction) => (
-        isTransactionBeforeDate(transaction, endDate)
+  
+      // Filter transactions by selected shared accounts
+      const sharedAccountIds = selectedAccounts.map((acct) => acct.accountId);
+      const filteredTransactions = transactionsSinceStartDate.filter((transaction) => (
+        sharedAccountIds.includes(transaction.account_id)
+        && isTransactionBeforeDate(transaction, endDate)
         && transaction.approved
         && !isTransactionATransfer(transaction)
       ));
-
+  
       setClassifiedTransactions(classifyTransactions({
-        displayedTransactions,
+        displayedTransactions: filteredTransactions,
         selectedAccounts,
         selectedParentCategories,
       }));
@@ -604,8 +607,8 @@ const CostSharingForYnab = () => {
             <input
               type="range"
               id="split-percentage-slider"
-              min="1"
-              max="99"
+              min="0"
+              max="100"
               value={myShare}
               onChange={(e) => setMyShare(e.target.value)}
             />
