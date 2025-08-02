e44f6487f4b12a2d91882634b9416ddd717e0ef8
Chelsea Schmidt <63372749+chelseaSchmidt@users.noreply.github.com>
2025-08-02 12:29:33 -0700

    Miscellaneous cleanup (#65)

--- landingPageWebpack.prod.js
+++ landingPageWebpack.prod.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require('webpack-merge');
-const common = require('./landingPageWebpack.common.js');
+const common = require('./landingPageWebpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
