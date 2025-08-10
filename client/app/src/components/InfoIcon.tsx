7fc33299917f21382085cd8bc7b1b2872ff98a5b
Chelsea Schmidt <63372749+chelseaSchmidt@users.noreply.github.com>
2025-08-10 16:04:14 -0700

    Position info icon tooltips based on available window space (#144)

--- client/app/src/components/InfoIcon.tsx
+++ client/app/src/components/InfoIcon.tsx
@@ -1,10 +1,34 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode, useRef, useState } from 'react';
 import styled from 'styled-components';
 import colors from '../../../shared/colors';
 import zIndices from '../../../shared/zIndices';
+import { isDefined } from './utils/general';
+
+const TOOLTIP_WIDTH = 200;
+const DEFAULT_MARGIN = 20;
+
+type Bounds = {
+  top?: number;
+  bottom?: number;
+  left?: number;
+  right?: number;
+};
+
+interface TooltipState {
+  isOpen: boolean;
+  shouldOpenUpward: boolean;
+  shouldOpenLeft: boolean;
+  positions?: Bounds;
+  margins?: Bounds;
+}
+
+/**
+ * STYLED COMPONENTS
+ */
 
 const Container = styled.div<{ $color: string }>`
   position: relative;
+  box-sizing: content-box;
   display: inline-flex;
   align-items: center;
   justify-content: center;
@@ -18,30 +42,63 @@ const Container = styled.div<{ $color: string }>`
   font-family: Arial, Helvetica, sans-serif;
   border: 1px solid ${({ $color }) => $color};
   color: ${({ $color }) => $color};
-
-  &:hover {
-    && div {
-      display: block;
-    }
-  }
 `;
 
-const Tooltip = styled.div<{ $isOpen: boolean }>`
-  display: ${({ $isOpen }) => ($isOpen ? 'block' : 'none')};
+/**
+ * toggle `visibility` instead of `display` to keep height property always available
+ */
+const Tooltip = styled.div<{ $tooltipState: TooltipState }>`
   box-sizing: border-box;
   position: absolute;
   z-index: ${zIndices.tooltip};
   top: 100%;
   left: 0;
-  width: 300px;
-  padding: 5px;
-  border-radius: 5px;
+  width: ${TOOLTIP_WIDTH}px;
+  padding: 10px;
+  border-radius: 3px;
   background: #444;
   color: white;
   font-size: 12px;
   font-weight: normal;
   white-space: wrap;
+
+  ${({ $tooltipState: { shouldOpenUpward, positions = {}, margins = {} } }) =>
+    shouldOpenUpward && isDefined(positions.bottom)
+      ? `
+          top: unset;
+          bottom: 100%;
+          max-height: calc(${positions.bottom}px - ${margins.top || DEFAULT_MARGIN}px);
+        `
+      : isDefined(positions.top)
+      ? `
+          top: 100%;
+          max-height: calc(100vh - ${positions.top}px - ${margins.bottom || DEFAULT_MARGIN}px);
+        `
+      : ''}
+
+  ${({ $tooltipState: { shouldOpenLeft, positions = {}, margins = {} } }) =>
+    shouldOpenLeft && isDefined(positions.right)
+      ? `
+          left: unset;
+          right: 0;
+          max-width: calc(${positions.right}px - ${margins.left || DEFAULT_MARGIN}px);
+        `
+      : isDefined(positions.left)
+      ? `
+          left: 0;
+          max-width: calc(100vw - ${positions.left}px - ${margins.right || DEFAULT_MARGIN}px);
+        `
+      : ''}
+
+  ${({ $tooltipState: { isOpen } }) =>
+    isOpen
+      ? 'visibility: visible;'
+      : 'visibility: hidden; position: fixed; z-index: 0; top: 0; left: 0;'};
 `;
+
+/**
+ * MAIN
+ */
 
 interface Props {
   tooltipContent?: ReactNode;
@@ -54,16 +111,82 @@ export default function InfoIcon({
   theme = 'info',
   color = colors.infoIcon,
 }: Props) {
-  const [isTooltipOpen, setIsTooltipOpen] = useState(false);
+  const [tooltipState, setTooltipState] = useState<TooltipState>({
+    isOpen: false,
+    shouldOpenUpward: false,
+    shouldOpenLeft: false,
+    positions: {},
+    margins: {},
+  });
 
-  const symbol = theme === 'error' ? '!' : '?';
-
-  const toggleTooltip = () => setIsTooltipOpen((isOpen) => !isOpen);
+  const iconRef = useRef<HTMLDivElement>(null);
+  const tooltipRef = useRef<HTMLDivElement>(null);
 
   return (
-    <Container $color={color} onClick={toggleTooltip} onMouseLeave={() => setIsTooltipOpen(false)}>
-      {symbol}
-      {tooltipContent && <Tooltip $isOpen={isTooltipOpen}>{tooltipContent}</Tooltip>}
+    <Container
+      $color={color}
+      ref={iconRef}
+      onMouseLeave={() => setTooltipState((prev) => ({ ...prev, isOpen: false }))}
+      onMouseEnter={() =>
+        setTooltipState((prev) => ({
+          ...prev,
+          isOpen: true,
+          ...getTooltipLocation({ iconRef, tooltipRef }),
+        }))
+      }
+      onClick={() =>
+        setTooltipState((prev) => {
+          const isOpen = !prev.isOpen;
+          return {
+            ...prev,
+            isOpen,
+            ...(isOpen ? getTooltipLocation({ iconRef, tooltipRef }) : {}),
+          };
+        })
+      }
+    >
+      {theme === 'error' ? '!' : '?'}
+
+      {tooltipContent && (
+        <Tooltip $tooltipState={tooltipState} ref={tooltipRef}>
+          {tooltipContent}
+        </Tooltip>
+      )}
     </Container>
   );
+}
+
+function getTooltipLocation(args: {
+  iconRef: React.RefObject<HTMLDivElement | null>;
+  tooltipRef: React.RefObject<HTMLDivElement | null>;
+  margins?: Bounds;
+}): Omit<TooltipState, 'isOpen'> {
+  const HEIGHT_ESTIMATE = 100;
+
+  const { iconRef, tooltipRef, margins } = args;
+
+  const tooltipBounds = tooltipRef.current?.getBoundingClientRect();
+  const iconBounds = iconRef.current?.getBoundingClientRect();
+  const tooltipHeight = tooltipBounds?.height;
+  const tooltipTop = iconBounds?.bottom;
+  const tooltipLeft = iconBounds?.left;
+
+  return {
+    shouldOpenUpward:
+      (isDefined(tooltipTop) ? tooltipTop : 0) >
+      window.innerHeight -
+        (tooltipHeight || HEIGHT_ESTIMATE) -
+        (isDefined(margins?.bottom) ? margins.bottom : DEFAULT_MARGIN),
+    shouldOpenLeft:
+      (isDefined(tooltipLeft) ? tooltipLeft : 0) >
+      window.innerWidth -
+        TOOLTIP_WIDTH -
+        (isDefined(margins?.right) ? margins.right : DEFAULT_MARGIN),
+    positions: {
+      top: tooltipTop,
+      bottom: iconBounds?.top,
+      left: tooltipLeft,
+      right: iconBounds?.right,
+    },
+  };
 }
--- client/app/src/components/utils/general.ts
+++ client/app/src/components/utils/general.ts
@@ -1,1 +1,3 @@
 export const toId = <T, I>(item: T & { id: I }) => item.id;
+
+export const isDefined = <T>(value: T) => value !== null && value !== undefined;
