1801170f179f4004a57bff73519ae5b7ce62b436
Chelsea Schmidt <63372749+chelseaSchmidt@users.noreply.github.com>
2025-09-12 15:04:58 -0700

    Miscellaneous code cleanup (#158)

--- client/app/src/components/utils/general.ts
+++ client/app/src/components/utils/general.ts
@@ -1,3 +1,23 @@
 export const toId = <T, I>(item: T & { id: I }) => item.id;
 
 export const isDefined = <T>(value: T) => value !== null && value !== undefined;
+
+export const isNumber = <T>(value: T): value is T & number => typeof value === 'number';
+
+export const isNonNullObject = <T>(value: T): value is T & object =>
+  typeof value === 'object' && value !== null;
+
+export const hasResponseAndStatus = <T>(
+  value: T,
+): value is T & { response: { status: number } } => {
+  return (
+    isNonNullObject(value) &&
+    'response' in value &&
+    isNonNullObject(value.response) &&
+    'status' in value.response &&
+    typeof value.response.status === 'number'
+  );
+};
+
+export const hasMessage = <T>(value: T): value is T & { message: string } =>
+  isNonNullObject(value) && 'message' in value && typeof value.message === 'string';
