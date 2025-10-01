63117393eca8f6d106f9e7adf1f2091bdd5b31ed
Chelsea Schmidt <63372749+chelseaSchmidt@users.noreply.github.com>
2025-09-30 18:53:11 -0700

    Make cost inputs section a proper form and refactor iou params tile (#198)

---
+++ client/app/src/components/CostSplittingForm/IouTransactionParams/SubmitButton.tsx
@@ -0,0 +1,44 @@
+import { Button } from '../../../../../shared/styledComponents';
+import InfoIcon from '../../InfoIcon';
+import { SubmittingSpinner } from '../../styledComponents';
+
+interface Props {
+  submitting: boolean;
+  submit: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => Promise<void>;
+  disabled: boolean;
+  selectedIds: Set<string | number>;
+  accountId: string;
+}
+
+export default function SubmitButton({
+  submitting,
+  submit,
+  disabled,
+  selectedIds,
+  accountId,
+}: Props) {
+  return (
+    <Button type="submit" onClick={submit} disabled={disabled}>
+      {submitting ? (
+        <SubmittingSpinner role="progressbar" aria-label="Submitting" />
+      ) : disabled ? (
+        <>
+          Split costs&nbsp;
+          <InfoIcon color="white" tooltipContent={getTooltip(selectedIds, accountId)} />
+        </>
+      ) : (
+        'Split Costs'
+      )}
+    </Button>
+  );
+}
+
+function getTooltip(selectedIds: Set<string | number>, accountId: string): string {
+  return !selectedIds.size && !accountId
+    ? 'Please select an IOU account and at least one transaction to split'
+    : !selectedIds.size
+    ? 'Please select at least one transaction to split'
+    : !accountId
+    ? 'Please select an IOU account'
+    : '';
+}
